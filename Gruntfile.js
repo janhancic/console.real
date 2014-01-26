4742eb03f2886bfa136a6c264f73b815bb1a11f9
Jan Hancic <jan.hancic@gmail.com>
2014-01-26 17:06:23 +0000

    remove jasmine tests (for now)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,4 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
-		jasmine : {
-			src : 'src/console.real.js',
-			options: {
-				specs : 'specs/console.real.spec.js'
-			}
-		},
-		watch: {
-			files: ['src/console.real.js', 'specs/console.real.spec.js'],
-			tasks: 'jasmine'
-		}
 	});
-
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks('grunt-contrib-watch');
 };
