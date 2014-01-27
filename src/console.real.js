41a0734a43ebc26b8775a2e0101bd50677b70177
Jan Hancic <jan.hancic@gmail.com>
2014-01-27 21:43:26 +0000

    use proper object.clone method instead of JSON stringify/parse

--- src/console.real.js
+++ src/console.real.js
@@ -1,6 +1,95 @@
 ;(function (console, JSON) {
 'use strict';
 
+// code based on https://github.com/pvorb/node-clone
+var clone = (function () {
+	var cloneUtil = {
+		isArray: function (ar) {
+			return Array.isArray(ar) || (typeof ar === 'object' && Object.prototype.toString.call(ar) === '[object Array]');
+		},
+		isDate: function (d) {
+			return typeof d === 'object' && Object.prototype.toString.call(d) === '[object Date]';
+		},
+		isRegExp: function (re) {
+			return typeof re === 'object' && Object.prototype.toString.call(re) === '[object RegExp]';
+		},
+		getRegExpFlags: function (re) {
+			var flags = '';
+			re.global && (flags += 'g');
+			re.ignoreCase && (flags += 'i');
+			re.multiline && (flags += 'm');
+			return flags;
+		}
+	};
+
+	function clone(parent) {
+		var useBuffer = typeof Buffer !== 'undefined',
+			circularParent = {},
+			circularResolved = {},
+			circularReplace = [];
+
+		function _clone(parent, context, child, cIndex) {
+			var i;
+			if (typeof parent == 'object') {
+				if (parent == null) {
+					return parent;
+				}
+
+				for (i in circularParent) {
+					if (circularParent[i] === parent) {
+						circularReplace.push({'resolveTo': i, 'child': child, 'i': cIndex});
+						return null;
+					}
+				}
+
+				circularParent[context] = parent;
+				if (cloneUtil.isArray(parent)) {
+					child = [];
+					for(i in parent) {
+						child[i] = _clone(parent[i], context + '[' + i + ']', child, i);
+					}
+				} else if (cloneUtil.isDate(parent)) {
+					child = new Date(parent.getTime());
+				} else if (cloneUtil.isRegExp(parent)) {
+					child = new RegExp(parent.source, cloneUtil.getRegExpFlags(parent));
+					if (parent.lastIndex) {
+						child.lastIndex = parent.lastIndex;
+					}
+				} else if (useBuffer && Buffer.isBuffer(parent)) {
+					child = new Buffer(parent.length);
+					parent.copy(child);
+				} else {
+					child = {};
+					child.__proto__ = parent.__proto__;
+					for(i in parent) {
+						child[i] = _clone(parent[i], context + '[' + i + ']', child, i);
+					}
+				}
+
+				circularResolved[context] = child;
+			} else {
+				child = parent;
+			}
+
+			return child;
+		}
+
+		var i,
+			cloned = _clone(parent, '*');
+
+		for (i in circularReplace) {
+			var c = circularReplace[i];
+			if (c && c.child && c.i in c.child) {
+				c.child[c.i] = circularResolved[c.resolveTo];
+			}
+		}
+
+		return cloned;
+	}
+
+	return clone;
+}());
+
 if (typeof console.real !== 'undefined') {
 	throw new Error('console.real cannot be run, as something else is occupying the `console.real` object.')
 }
@@ -19,9 +108,7 @@ consoleFunctionNames.forEach(function (consoleFunctionName) {
 			return;
 		}
 
-		var args = Array.prototype.slice.call(arguments).map(function(argument) {
-			return JSON.parse(JSON.stringify(argument));
-		});
+		var args = Array.prototype.slice.call(arguments).map(clone);
 
 		originalConsoleFunctions[consoleFunctionName].apply(console, args);
 	};
