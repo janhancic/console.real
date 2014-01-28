060e75123079f8cf5e51a190564a81085ad4763a
Jan Hancic <jan.hancic@gmail.com>
2014-01-28 23:26:46 +0000

    Use repository url from package.json for minified version header comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> (https://github.com/janhancic/console.real) */\n'
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> (<%= pkg.repository.url %>) */\n'
 			},
 			build: {
 				src: 'src/console.real.js',
